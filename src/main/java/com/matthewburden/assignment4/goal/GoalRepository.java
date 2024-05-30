9e60379724402269b3120547fc9e606900295974
Matthew Burden <mdburden@uncg.edu>
2024-05-30 12:04:49 -0400

    Assignment 4: UI Changes

--- src/main/java/com/matthewburden/assignment4/goal/GoalRepository.java
+++ src/main/java/com/matthewburden/assignment4/goal/GoalRepository.java
@@ -1,8 +1,15 @@
 package com.matthewburden.assignment4.goal;
 
+import com.matthewburden.assignment4.task.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GoalRepository extends JpaRepository<Goal,Integer> {
+    @Query(value = "select * from goal where user_id = :userId", nativeQuery = true)
+    public List<Task> getUserGoals(@Param("userId") int userId);
 }
